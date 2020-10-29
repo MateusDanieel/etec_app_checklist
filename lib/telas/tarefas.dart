3a8710340c4a319f44a3ed7c53bc5117dc08c774
Mateus D. Silva <msilva@CORP.TRITONE>
2020-10-29 20:29:53 -0300

    Finalização da aplicação, funções finais.

--- lib/telas/tarefas.dart
+++ lib/telas/tarefas.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
 
 class Tarefas extends StatefulWidget {
   @override
@@ -7,19 +11,48 @@ class Tarefas extends StatefulWidget {
 
 class _TarefasState extends State<Tarefas> {
   List _tarefa = [];
+  Map<String, dynamic> _lastRemoved;
+  int _lastRemovedPos;
 
   final _tarefaController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    _readData().then((data) {
+      setState(() {
+        _tarefa = json.decode(data);
+      });
+    });
+  }
 
   void addTarefa() {
     setState(() {
       Map<String, dynamic> novaTarefa = Map();
       novaTarefa["title"] = _tarefaController.text;
       novaTarefa["ok"] = false;
-
       _tarefaController.text = "";
-
       _tarefa.add(novaTarefa);
+      _saveData();
     });
+  }
+
+//criar a funcao refesh
+  Future<Null> _refresh() async {
+    await Future.delayed(Duration(seconds: 1));
+    setState(() {
+      _tarefa.sort((a, b) {
+        if (a["ok"] && !b["ok"])
+          return 1;
+        else if (!a["ok"] && b["ok"])
+          return -1;
+        else
+          return 0;
+        _saveData();
+      });
+    });
+
+    return null;
   }
 
   @override
@@ -45,33 +78,93 @@ class _TarefasState extends State<Tarefas> {
                 ),
                 RaisedButton(
                   color: Colors.blueAccent,
-                  child: Text("Inserir"),
+                  child: Text("ADD"),
                   textColor: Colors.white,
                   onPressed: addTarefa,
                 ),
               ])),
           Expanded(
-            child: ListView.builder(
-                padding: EdgeInsets.only(top: 10.0),
-                itemCount: _tarefa.length,
-                itemBuilder: (context, index) {
-                  return CheckboxListTile(
-                    title: Text(_tarefa[index]['title']),
-                    value: _tarefa[index]['ok'],
-                    secondary: CircleAvatar(
-                      child: Icon(
-                          _tarefa[index]['ok'] ? Icons.check : Icons.error),
-                    ),
-                    onChanged: (c) {
-                      setState(() {
-                        _tarefa[index]['ok'] = c;
-                      });
-                    },
-                  );
-                }),
+            child: RefreshIndicator(
+              onRefresh: _refresh,
+              child: ListView.builder(
+                  padding: EdgeInsets.only(top: 10.0),
+                  itemCount: _tarefa.length,
+                  itemBuilder: buildItem),
+            ),
           )
         ],
       ),
     );
+  }
+
+  Widget buildItem(context, index) {
+    return Dismissible(
+      key: Key(DateTime.now().millisecondsSinceEpoch.toString()),
+      background: Container(
+        color: Colors.red,
+        child: Align(
+          alignment: Alignment(-0.9, 0.0),
+          child: Icon(
+            Icons.delete,
+            color: Colors.blue,
+          ),
+        ),
+      ),
+      // direction: DismissDirection.startToEnd, // SE NÃO COLOCAR O DIRECTION, ELE VAI APAGAR DESLIZANDO PARA QUALQUER LADO
+      child: CheckboxListTile(
+        title: Text(_tarefa[index]['title']),
+        value: _tarefa[index]['ok'],
+        secondary: CircleAvatar(
+          child: Icon(_tarefa[index]['ok'] ? Icons.check : Icons.error),
+        ),
+        onChanged: (c) {
+          setState(() {
+            _tarefa[index]['ok'] = c;
+            _saveData();
+          });
+        },
+      ),
+      onDismissed: (direction) {
+        _lastRemoved = Map.from(_tarefa[index]);
+        _lastRemovedPos = index;
+        _tarefa.removeAt(index);
+        _saveData();
+
+        final snack = SnackBar(
+          content: Text("Tarefa '${_lastRemoved["title"]}' removida!"),
+          action: SnackBarAction(
+            label: "Desfazer",
+            onPressed: () {
+              setState(() {
+                _tarefa.insert(_lastRemovedPos, _lastRemoved);
+                _saveData();
+              });
+            },
+          ),
+          duration: Duration(seconds: 4),
+        );
+        Scaffold.of(context).showSnackBar(snack);
+      },
+    );
+  }
+
+  Future<File> _getFile() async {
+    final directory = await getApplicationDocumentsDirectory();
+    return File("${directory.path}​​/data.json");
+  }
+
+  Future<File> _saveData() async {
+    String data = json.encode(_tarefa);
+    final file = await _getFile();
+    return file.writeAsString(data);
+  }
+
+  Future<String> _readData() async {
+    try {
+      final file = await _getFile();
+      return file.readAsString();
+    } catch (e) {
+      return null;
+    }
   }
 }
